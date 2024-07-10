f9787e3fb60c8fa9bddb724ca7261a7784831c8a
Akhyar25s <akhyarrr25@gmail.com>
2024-07-10 15:24:19 +0800

    add : mengganti jadi naga

--- uas.cpp
+++ uas.cpp
@@ -11,8 +11,8 @@ struct Kartu {
 
 int main() {
     Kartu kartu1;
-    kartu1.nama = "Blue-Eyes White Dragon";
-    kartu1.deskripsi = "This legendary dragon is a powerful engine of destruction. Virtually invincible, very few have faced this awesome creature and lived to tell the tale.";
+    kartu1.nama = "Obelisk the tormentor";
+    kartu1.deskripsi = "naga";
     kartu1.attack = 3000;
     kartu1.defend = 2500;
 
