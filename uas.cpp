6977768d3b12d82c6f953a2e92ec29b743fac67e
Akhyar25s <akhyarrr25@gmail.com>
2024-07-10 15:33:41 +0800

    add: mengcombo kartu

--- uas.cpp
+++ uas.cpp
@@ -21,5 +21,17 @@ int main() {
     cout << "Attack: " << kartu1.attack << endl;
     cout << "Defend: " << kartu1.defend << endl;
 
+    Kartu kartu2;
+    kartu2.nama = "sky dragon";
+    kartu2.deskripsi = "naga";
+    kartu2.attack = 9000;
+    kartu2.defend = 8000;
+
+    cout << "Nama: " << kartu2.nama << endl;
+    cout << "Deskripsi: " << kartu2.deskripsi << endl;
+    cout << "Attack: " << kartu2.attack << endl;
+    cout << "Defend: " << kartu2.defend << endl;
+
+
     return 0;
 }
